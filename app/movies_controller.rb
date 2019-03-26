22a41484f39d8fb7061285f42e987a53a38f5ee0
Joan Lyness <joanrigdon@gmail.com>
2019-03-26 01:46:30 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,7 +28,6 @@ def can_be_created_in_a_block(title="Home Alone", release_date=1990)
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  binding.pry
   Movie.create do |m|
     m.title = title
     m.release_date = release_date
